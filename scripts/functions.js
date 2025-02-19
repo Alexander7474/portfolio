d5df95071be4b1bc172f69986a21deaceabfab40
Alexander7474 <alexandre.lanternier@outlook.fr>
2025-02-19 18:23:29 +0100

    gestion auto des fenêtre supplémentaire

--- scripts/functions.js
+++ scripts/functions.js
@@ -12,3 +12,39 @@ function updateTime() {
   document.getElementById('time').textContent = `${hours}:${minutes}`;
 }
 
+function createWindow() {
+  //creation de la fenêtre 
+  const win = document.createElement("div");
+  win.classList.add("window");
+
+  //creation de la top bar de la fenetre 
+  const topWin = document.createElement("div");
+  topWin.classList.add("top-window");
+
+  //creation du body de la fenêtre 
+  const bodyWin = document.createElement("div");
+  bodyWin.classList.add("body-window");
+
+  //creation des trois boutons de la top bar 
+  let buttons = [];
+  for(let i = 0; i < 3; i++){
+    const b = document.createElement("div");
+    b.classList.add("icon-anim");
+    b.classList.add("circular-button");
+    buttons.push(b);
+  }
+
+  buttons[0].classList.add("hide-button");
+  buttons[1].classList.add("resize-button");
+  buttons[2].classList.add("close-button");
+
+  for (let i = 0; i < 3; i++) {
+    topWin.appendChild(buttons[i]);
+  }
+
+  win.appendChild(topWin);
+  win.appendChild(bodyWin);
+  
+  return win;
+}
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,43 @@
-
 // Mettre à jour l'heure toutes les secondes
 setInterval(updateTime, 1000);
 
 // Initialiser l'heure dès le début
 updateTime();
+
+// getion des tiles
+let winCount = 1; // Compteur de tile
+   
+//listener sur la list d'icon dans la bar de navigation
+document.querySelectorAll(".nav-icon").forEach(div => {
+  div.addEventListener("click", function() {
+    const name = div.querySelector("img").getAttribute("alt");
+
+    const win = createWindow();
+    const bodyWin = win.querySelector(".body-window");
+
+    if(name == "Me"){ // Page personnel
+
+      bodyWin.innerHTML = "<h1>Alexandre LANTERNIER</h1>";
+      bodyWin.innerHTML += "<p>Bienvenue sur mon portfolio</p>";
+
+    }else if(name == "Git"){ //Page github
+
+    }else if(name == "HTB"){ //Page Hack the box
+      bodyWin.innerHTML = "<p>Mon profil Hack the box</p>";
+      bodyWin.innerHTML += "<a href='https://app.hackthebox.com/profile/384315'><img src='https://www.hackthebox.com/badge/image/384315'></a>";
+    }
+
+    document.getElementById("windows-conteneur").appendChild(win);
+  });
+});
+
+//listenner sur le conteneur de fenêtre
+document.getElementById("windows-conteneur").addEventListener("click", (event) => {
+  // Vérifier si l'élément cliqué est une div avec la classe "carre"
+  if (event.target && event.target.classList.contains("close-button")) {
+    event.target.parentElement.parentElement.remove();
+  }
+  if (event.target && event.target.classList.contains("hide-button")) {
+    event.target.parentElement.parentElement.remove();
+  }
+});
